a265ce2f176ece3b61b999580338331d5614319e
setezzy <1020146313@qq.com>
2017-10-25 19:10:56 +0800

    add activity

---
+++ Project/src/com/itau/mygod/ui/SearchActivity.java
@@ -0,0 +1,48 @@
+package com.itau.mygod.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+import com.itau.mygod.R;
+import com.itau.mygod.ui.base.BaseActivity;
+import com.itau.mygod.utils.CommonTools;
+import com.itau.mygod.widgets.AutoClearEditText;
+
+
+public class SearchActivity extends BaseActivity {
+
+	private AutoClearEditText mEditText = null;
+	private ImageButton mImageButton = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search);
+		findViewById();
+		initView();
+	}
+
+	@Override
+	protected void findViewById() {
+		// TODO Auto-generated method stub
+		mEditText = (AutoClearEditText) findViewById(R.id.search_edit);
+		mImageButton = (ImageButton) findViewById(R.id.search_button);
+	}
+
+	@Override
+	protected void initView() {
+		// TODO Auto-generated method stub
+		mEditText.requestFocus();
+		mImageButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				CommonTools.showShortToast(SearchActivity.this, "亲，该功能暂未开放");
+			}
+		});
+	}
+}
