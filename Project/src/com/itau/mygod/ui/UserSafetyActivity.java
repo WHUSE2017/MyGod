db8f2ee336eb9bb6b6098d33d36cd85be0426583
Cyannn <wtl0726@126.com>
2017-11-02 22:57:51 +0800

    add function: modify password and exit

--- Project/src/com/itau/mygod/ui/UserSafetyActivity.java
+++ Project/src/com/itau/mygod/ui/UserSafetyActivity.java
@@ -1,6 +1,8 @@
 package com.itau.mygod.ui;
 
+
 import com.itau.jingdong.R;
+import com.itau.mygod.bean.Constants;
 import com.itau.mygod.ui.base.BaseActivity;
 
 import android.content.Intent;
@@ -8,12 +10,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserSafetyActivity extends BaseActivity implements OnClickListener {
 	private static final String Tag="UserSafetyActivity";
 	private UserSafetyActivity userSafetyActivity=null;
 	private Intent mIntent;
 	private LinearLayout modify_passewd_bt;
+	private LinearLayout modify_phone_bt;
+	private LinearLayout setting_check_update_bt;
+	private LinearLayout setting_cancel_bt;
+	private TextView user_name;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +38,20 @@ public class UserSafetyActivity extends BaseActivity implements OnClickListener 
 	protected void findViewById() {
 		// TODO Auto-generated method stub
 		modify_passewd_bt = (LinearLayout) findViewById(R.id.setting_modify_passwd);
-		
+		modify_phone_bt = (LinearLayout) findViewById(R.id.setting_modify_phone);
+		setting_check_update_bt = (LinearLayout) findViewById(R.id.setting_check_update);
+		setting_cancel_bt = (LinearLayout) findViewById(R.id.setting_cancel);
+		user_name = (TextView) findViewById(R.id.user_info_name);
+		user_name.setText(Constants.userobject.getName());
 	}
 	
 	@Override
 	protected void initView() {
 		// TODO Auto-generated method stub
 		modify_passewd_bt.setOnClickListener(this);
+		modify_phone_bt.setOnClickListener(this);
+		setting_check_update_bt.setOnClickListener(this);
+		setting_cancel_bt.setOnClickListener(this);
 		
 	}
 
@@ -48,7 +63,26 @@ public class UserSafetyActivity extends BaseActivity implements OnClickListener 
 		mIntent=new Intent(UserSafetyActivity.this, SetPasswordActivity.class);
 		startActivity(mIntent);			
 		break;
+		
+	case R.id.setting_modify_phone:
+		mIntent=new Intent(UserSafetyActivity.this, SetPhoneActivity.class);
+		startActivity(mIntent);			
+		break;
+		
+	case R.id.setting_check_update:
+		Toast.makeText(UserSafetyActivity.this,"当前已是最新版本",Toast.LENGTH_SHORT).show();
+		break;
+		
+	case R.id.setting_cancel:
+		Intent logoutIntent = new Intent();
+		logoutIntent.setClass(UserSafetyActivity.this,PersonalActivity.class);
+		logoutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+		Constants.status = false;
+        startActivity(logoutIntent);
+		
 		}
+		}
+	
 		
 	}
 
@@ -56,4 +90,3 @@ public class UserSafetyActivity extends BaseActivity implements OnClickListener 
 
 	
 
-}
